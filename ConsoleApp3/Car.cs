2a9ea2cea48be108bc2963e1f93b0adaacbaf08d
Iván López Saito <ivan.lopez.saito@gmail.com>
2018-01-24 12:49:37 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Car
+    {
+        private String type { get; set; }
+        private int numTires { get; set; }
+        private int year { get; set; }
+        private Boolean runs { get; set; }
+
+        public Car() { }
+
+        public Car(String type, int numTires, int year, bool runs)
+        {
+            this.type = type;
+            this.numTires = numTires;
+            this.year = year;
+            this.runs = runs;
+        }
+
+        
+
+        public string printInfo()
+        {
+            String info, engine;
+            if (this.runs)
+            {
+                engine = "runing";
+            }
+            else
+            {
+                engine = "off";
+            }
+            info = "Type of car: " + this.type + " number of tires: " + this.numTires.ToString() + " made in: " + this.year.ToString() + " engine " + engine;
+
+            Console.WriteLine(info);
+
+            return info;
+        }
+    }
+}
---
+++ ConsoleApp3/Tests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //listas
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(1);
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+            primeNumbers.Add(11);
+
+            List<string> food = new List<string>();
+            food.Add("apple");
+            food.Add("banana");
+
+            List<string> vegetables = new List<string>();
+            vegetables.Add("tomato");
+            vegetables.Add("carrot");
+
+            //concatenar listas
+            food.AddRange(vegetables);
+
+            //dictionaries
+            Dictionary<String, String> apple = new Dictionary<string, string>();
+            apple.Add("Apple", "Fuji");
+            apple.Add("Apple1", "Golden");
+            apple.Add("Apple2", "Pink lady");
+
+            Dictionary<String, int> oranges = new Dictionary<string, int>();
+            oranges.Add("orange", 1);
+            oranges.Add("orange1", 2);
+            oranges.Add("orange2", 3);
+            oranges.Add("orange3", 4);
+            oranges.Add("orange4", 5);
+
+            Dictionary<String, String> banana = new Dictionary<string, string>();
+            banana.Add("banana", "canario");
+            banana.Add("banana1", "no canario");
+
+            //strings
+            String firstName, lastName, age, sentence;
+            firstName = "John";
+            lastName = "Doe";
+            age = "27";
+            sentence = firstName + " " + lastName + " is " + age + " years old.";
+
+            //loop for
+            int X;
+            Console.WriteLine("For: ");
+            for (X = 0 ;X < 6;X++)
+            {
+                Console.WriteLine(X);
+            }
+
+            //while loop
+            Console.WriteLine("While: ");
+            X = 0;
+            while (X < 6)
+            {
+                Console.WriteLine(X);
+                X++;
+            }
+
+            //methods
+            int a = 10, b = 20, result;
+
+            result = DivideTheNumbers(a, b);
+            Console.WriteLine("result: " + result);
+
+            //classes
+
+            Car car = new Car("car", 4, 2000, true);
+            car.printInfo();
+
+            Car coldCar = new Car("car", 4, 1980, true);
+            coldCar.printInfo();
+
+            String opcion = "";
+            while(opcion != "out")
+            {
+                Console.WriteLine("To close the console write out.");
+                try
+                {
+                    opcion = Console.ReadLine();
+                }
+                catch
+                {
+
+                }
+                
+            }
+        }
+
+        public static int DivideTheNumbers(int a, int b)
+        {
+            return b / a;
+        }
+    }
+}
